73f8ee6b3d6e2d877781db5d407f04f89b698435
0x6e <0x6e@users.noreply.github.com>
2016-03-28 15:38:34 +0100

    Started UDP searching, but it doesn't work yet

--- controller/app/js/RoverManagerFactory.js
+++ controller/app/js/RoverManagerFactory.js
@@ -8,6 +8,19 @@ factories.factory('RoverManager', [function RoverManagerFactory() {
 
   function RoverManager() {
     console.log("RoverManager created");
+    
+    // Create the Socket
+    chrome.sockets.udp.create({}, function(socketInfo) {
+    
+      // The socket is created, now we can send some data
+      var socketId = socketInfo.socketId;
+      var arrayBuffer = new ArrayBuffer(12);
+      chrome.sockets.udp.send(socketId, arrayBuffer,'127.0.0.1', 1337,
+        function(sendInfo) {
+          console.log("sent " + sendInfo.bytesSent);
+        });
+    });
+
   };
 
   return RoverManager;
