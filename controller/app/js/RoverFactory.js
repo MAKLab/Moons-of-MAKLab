88d21759d5bf141380cb02d1067149ff1965ef9e
0x6e <0x6e@users.noreply.github.com>
2016-03-29 20:42:42 +0100

    First pass at adding command functionality to the RoverFactory, RoverCtrl and rover.html

--- controller/app/js/RoverFactory.js
+++ controller/app/js/RoverFactory.js
@@ -19,6 +19,50 @@ factories.factory('Rover', ['$http', 'API_ENDPOINT', function RoverFactory($http
     return 'http://' + this.ip + '/video_feed';
   };
 
+
+  // Motor commands
+  Rover.prototype.forwardCommand = function(distance) {
+    return { "command": "forward", "distance": distance };
+  };
+
+  Rover.prototype.backCommand = function(distance) {
+    return { "command": "back", "distance": distance };
+  };
+
+  Rover.prototype.leftCommand = function(distance) {
+    return { "command": "left", "distance": distance };
+  };
+
+  Rover.prototype.rightCommand = function(distance) {
+    return { "command": "right", "distance": distance };
+  };
+
+
+  // Pan/tilt commands
+  Rover.prototype.panCommand = function(distance) {
+    return { "command": "pan", "distance": distance };
+  };
+
+  Rover.prototype.tiltCommand = function(distance) {
+    return { "command": "tilt", "distance": distance };
+  };
+
+  Rover.prototype.centerCameraCommand = function() {
+    console.log("centerCameraCommand not implemented!");
+    return { };
+  };
+
+
+  Rover.prototype.abort = function() {
+    console.log("abort not implemented!");
+  };
+
+
+  Rover.prototype.sendCommands = function(commands) {
+    console.log(this.ip, commands)
+    return true;
+  };
+
   return Rover;
 
 }]);
--- controller/app/js/controllers.js
+++ controller/app/js/controllers.js
@@ -2,5 +2,73 @@ var controllers = angular.module('controllers', []);
 
 controllers.controller('RoverCtrl', ['$scope', '$http', 'Rover',
   function ($scope, $http, Rover) {
-    $scope.rover = new Rover('192.168.0.7:5000');
+    // The rover we are controlling
+    var rover = new Rover('192.168.0.7:5000');
+
+    // Toggles whether we send a list of commands, or direct commands
+    var sendDirectCommands = true;
+
+    // The command list
+    var commandList = [];
+
+    var addCommand = function(command) {
+      if (sendDirectCommands) {
+        commandList.push(command);
+
+        if (rover.sendCommands(commandList))
+          commandList.length = 0;
+
+      } else {
+        commandList.push(command);
+      }
+    };
+
+    // Provide the camera stream to the HTML
+    $scope.cameraStream = rover.cameraStream();
+
+
+    // Motor controls
+    $scope.forward = function(distance) {
+      addCommand(rover.forwardCommand(distance));
+    };
+
+    $scope.back = function(distance) {
+      addCommand(rover.backCommand(distance));
+    };
+
+    $scope.left = function(distance) {
+      addCommand(rover.leftCommand(distance));
+    };
+
+    $scope.right = function(distance) {
+      addCommand(rover.rightCommand(distance));
+    };
+
+
+    // Pan/Tilt controls
+    $scope.pan = function(distance) {
+      addCommand(rover.panCommand(distance));
+    };
+
+    $scope.tilt = function(distance) {
+      addCommand(rover.tiltCommand(distance));
+    };
+
+    $scope.centerCamera = function() {
+      addCommand(rover.centerCameraCommand());
+    };
+
+
+    // Houston, we have a problem...
+    $scope.abort = function() {
+      rover.abort();
+    };
+
+    $scope.sendCommands = function() {
+      if (sendDirectCommands || commandList.length == 0)
+        return;
+
+      if (rover.sendCommands(commandList))
+        commandList.length = 0;
+    };
   }]);
